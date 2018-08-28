68261275698e083e3d927b5e1b0047b55dd57998
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-08-28 15:18:42 +0200

    Create emotes.php

---
+++ emotes.php
@@ -0,0 +1,17 @@
+<?php
+/*
+___________               __                 
+\_   _____/ _____   _____/  |_  ____   ______
+ |    __)_ /     \ /  _ \   __\/ __ \ /  ___/
+ |        \  Y Y  (  <_> )  | \  ___/ \___ \ 
+/_______  /__|_|  /\____/|__|  \___  >____  >
+        \/      \/                 \/     \/ 
+
+*/
+
+$line = str_replace(":kek:", '<img src="https://static.nulled.to/public/style_emoticons/default/kek.png"></img>', $line); // KEK emoji
+$line = str_replace(":thinkwell:", '<img src="https://static.nulled.to/public/style_emoticons/default/cant.png"></img>', $line); // ThinkWell emoji
+$line = str_replace(":doge:", '<img src="https://static.nulled.to/public/style_emoticons/default/doge.png"></img>', $line); // Doge emoji
+$line = str_replace(":lul:", '<img src="https://static.nulled.to/public/style_emoticons/default/LUL.png"></img>', $line); // LUL emoji
+$line = str_replace(":ohgod:", '<img src="https://static.nulled.to/public/style_emoticons/default/ohgod.png"></img>', $line); // OhGod emoji
+?>
